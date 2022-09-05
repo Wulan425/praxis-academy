39fabed654ef7a6595e2d193cdae89a6fd1074f3
Wulan425 <wulandary402057@gmail.com>
2022-09-05 11:55:13 +0700

    w4 d1

--- novice/03-05/app/main.py
+++ novice/03-05/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2 
 
 app = Flask(__name__)
@@ -19,13 +19,32 @@ def index():
         query = f"insert into buah(nama, detail) values('{nama}', '{detail}')"
         curs.execute(query)
         conn.commit()
-        curs.close()
-        conn.close()
 
         print(request.method)
     query = f"select * from buah"
     curs.execute(query)
     data = curs.fetchall()
+    # data = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Ahad"]
+    return render_template("index.html", context=data)
+
+@app.route("/detail/<buah_id>")
+def detail (buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="wulan425" 
+    )
+
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    curs.close()
+    conn.close()
+    print(data)
+    return render_template("detail.html", context=data)
+
     # data = ["pisang", "mangga", "salak"]
         
         # print(30*"=")
@@ -39,8 +58,22 @@ def index():
     # print(request.form.get("nama"))
     # print(request.form.get("detail"))
     # print(30*"=")
-    # data = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Ahad"]
-    return render_template("index.html", context=data)
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="wulan425" 
+    )
+
+    curs = conn.cursor()
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    return redirect ("/")
 
 if __name__ == "__main__":
     app.run()
